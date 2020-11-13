a5a3c900f9e96518e91b16ab9d3f9cd823e5e898
Menno den Engelse <menno@denengelse.nl>
2020-11-13 14:21:21 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<title>plakmuur</title>
+
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+	<script
+	src="https://code.jquery.com/jquery-3.2.1.min.js"
+	integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+	crossorigin="anonymous"></script>
+
+	<link rel="stylesheet" href="assets/styles.css" />
+
+
+
+	
+</head>
+<body>
+
+
+
+
+<div class="container-fluid">
+	<div class="row">
+		
+		<div class="col-md-4">
+			
+			<div class="doel1" style="margin-top: -8px;">
+				<h1>Uit in R'dam</h1>
+				<p>
+					<strong>Affiches uit het Stadsarchief verbinden met locaties, films, artiesten</strong>
+				</p>
+
+				<p>
+					De niet auteursrechtelijk beschermde affiches zijn, voor zover ze van afbeelding waren voorzien, geschraapt van de website van het Stadsarchief Rotterdam, omgezet naar RDF en gepubliceerd in  <a href="https://druid.datalegend.net/menno/rotterdamspubliek/graphs">Druid</a>
+				</p>
+				<p>
+					Dat zal vooral handmatig werk zijn, maar het is te overzien: er zijn zo'n 1700 affiches en ze staan allemaal onder elkaar in <a href="https://api.druid.datalegend.net/s/yH58nhyU2">deze query</a>
+				</p>
+				<br />
+				<br />
+				<br />
+			</div>
+
+		</div>
+		
+		<div class="col-md-4">
+			
+			<div class="doel2" style="margin-top: 16px; margin-left: -23px;">
+				<h1>Inventarisatie</h1>
+				<strong>Welke collecties bevatten (veel) affiches? In hoeverre zijn ze gedateerd? Ingedeeld op onderwerp? Op plaatsnaam?</strong>
+
+				<br />
+				<br />
+				<br />
+
+				<ul>
+					<li><a href="https://api.data.netwerkdigitaalerfgoed.nl/s/GHd-VG3ni">Museum voor Wereldculturen</a> (813)</li>
+					<li><a href="https://api.druid.datalegend.net/s/wO7jYmSnu">IISG</a> (114.269 waarvan de thumbnail 'open' is)</li>
+					<li><a href="https://api.druid.datalegend.net/s/Hi2xoHn6A">Stadsarchief Rotterdam</a> (1773 niet auteursrechtelijk beschermd)</li>
+				</ul>
+				
+
+				<br />
+				<br />
+				<br />
+				<br />
+			</div>
+
+		</div>
+		
+		<div class="col-md-4">
+			
+			<div class="doel3">
+				<h1>Plakmuur</h1>
+				<strong>Toon een muur met affiches uit een specifiek jaar, en als het even kan ook uit een specifieke stad</strong>
+
+				<br />
+				<br />
+				<br />
+				<br />
+				<br />
+				<br />
+				<br />
+				<br />
+				<br />
+				<br />
+				<br />
+			</div>
+
+		</div>
+	</div>
+	
+</div>
